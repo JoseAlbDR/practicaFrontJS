980c0c2ace298bad82cd1ff70db6e2bb839031aa
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-17 09:21:05 +0200

    feat: password match validator in register

--- Wallafake/register/registerController.js
+++ Wallafake/register/registerController.js
@@ -1,8 +1,5 @@
 import { registerUser } from './registerModel.js';
-import {
-  createCustomEvent,
-  dispatchEvent,
-} from '../utils/createCustomEvent.js';
+import { dispatchEvent } from '../utils/createCustomEvent.js';
 
 export const registerController = async (form) => {
   const formData = new FormData(form);
@@ -10,17 +7,26 @@ export const registerController = async (form) => {
   const user = {
     username: formData.get('username'),
     password: formData.get('password'),
+    repeatPassword: formData.get('repeatPassword'),
   };
 
   try {
-    await registerUser(user);
-    dispatchEvent(
-      'signup',
-      { type: 'success', message: 'User successfully created' },
-      form
-    );
-    window.location.href = 'login.html';
+    if (isValidPassword(password, repeatPassword)) {
+      await registerUser(user);
+      dispatchEvent(
+        'signup',
+        { type: 'success', message: 'User successfully created' },
+        form
+      );
+      window.location.href = 'login.html';
+    } else {
+      throw new Error('Passwords do not match');
+    }
   } catch (error) {
     dispatchEvent('signup', { type: 'error', message: error.message }, form);
   }
+};
+
+const isValidPassword = (password, repeatPassword) => {
+  return password === repeatPassword;
 };
