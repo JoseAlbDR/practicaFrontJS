90d80eaf1065bd835040a5bb31ade87f672c3584
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-18 13:23:51 +0200

    feat: new div witn product-content class

--- Wallafake/product-list/productsListView.js
+++ Wallafake/product-list/productsListView.js
@@ -8,10 +8,12 @@ export const buildProduct = (product) => {
   return `
   <a href="./product-detail.html?id=${product.id}">
     <img src="${product.image}" alt="product image"  class="img"/>
-    <span>Name: ${product.name}</span>
-    <span>Description: ${product.description}</span>
-    <p>Price: ${product.price}€</p>
-    <p class=${product.for.toLowerCase()}>${product.for}</p>
+    <div class="product-content">
+      <span>Name: ${product.name}</span>
+      <span>Description: ${product.description}</span>
+      <p>Price: ${product.price}€</p>
+      <p class=${product.for.toLowerCase()}>${product.for}</p>
+    </div>
   </a>
   `;
 };
