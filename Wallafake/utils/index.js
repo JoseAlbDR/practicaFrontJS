b063821d1fc5769cb572a51a2482c0c7c3cedabd
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-19 09:01:21 +0200

    feat: index for utils folder

---
+++ Wallafake/utils/index.js
@@ -0,0 +1,9 @@
+export { BASE_URL } from './const';
+export { disableForm } from './disableForm';
+export { enableForm } from './enableForm';
+export { decodeToken } from './decodeToken';
+export {
+  dispatchCustomEvent,
+  errorMessageEvent,
+  successMessageEvent,
+} from './customEvent';
