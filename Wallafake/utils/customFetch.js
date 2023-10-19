dd1c230baf49011ea11a8ea4ad11e7b424eeb10d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-19 09:18:59 +0200

    feat: post method for customFetch util

--- Wallafake/utils/customFetch.js
+++ Wallafake/utils/customFetch.js
@@ -8,8 +8,33 @@ export const customFetch = {
       const data = await response.json();
 
       if (!response.ok)
-        throw new Error('Error fetching products, try again later');
+        throw new Error(
+          data.message || 'Error fetching products, try again later'
+        );
 
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  },
+  post: async (endPoint, body) => {
+    const url = BASE_URL + endPoint;
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    };
+
+    try {
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(
+          data.message || 'Error creating product, try again later'
+        );
+      }
       return data;
     } catch (error) {
       throw error;
