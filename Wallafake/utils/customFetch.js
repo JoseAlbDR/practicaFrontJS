8ce55cbf30e2bbaf81aa78477fceb3b12322ee27
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-19 09:12:33 +0200

    feat: new util customFetch for CRUD operations agains sparrest backend

--- Wallafake/utils/customFetch.js
+++ Wallafake/utils/customFetch.js
@@ -1,10 +1,18 @@
-import { BASE_URL } from './const';
+import { BASE_URL } from './index.js';
 
-const customFetch = {
+export const customFetch = {
   get: async (endPoint) => {
     const url = BASE_URL + endPoint;
     try {
       const response = await fetch(url);
-    } catch (error) {}
+      const data = await response.json();
+
+      if (!response.ok)
+        throw new Error('Error fetching products, try again later');
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
   },
 };
