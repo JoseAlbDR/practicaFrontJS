99e7f637fa83fdaac8706489cc2977f999b42685
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-19 09:13:22 +0200

    refactor: use customFetch.get in productList and productDetail models

--- Wallafake/product-detail/productDetailModel.js
+++ Wallafake/product-detail/productDetailModel.js
@@ -1,13 +1,7 @@
+import { customFetch } from '../utils/index.js';
+
 export const getProduct = async (id) => {
-  const url = `http://localhost:8000/api/products/${id}?_expand=user`;
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    if (!response.ok) throw new Error('Product not found');
-
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  const url = `/api/products/${id}?_expand=user`;
+  const product = await customFetch.get(url);
+  return product;
 };
--- Wallafake/product-list/productListModel.js
+++ Wallafake/product-list/productListModel.js
@@ -1,14 +1,8 @@
+import { customFetch } from '../utils/index.js';
+
 export const getProducts = async () => {
-  const url = 'http://localhost:8000/api/products?_expand=user';
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+  const endPoint = '/api/products?_expand=user';
 
-    if (!response.ok)
-      throw new Error('Error fetching products, try again later');
-
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  const products = await customFetch.get(endPoint);
+  return products;
 };
