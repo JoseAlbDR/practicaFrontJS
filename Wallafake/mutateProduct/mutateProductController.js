28c2d7ba7248d4cd58755fde8ec994ac34da5e9e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-21 11:09:51 +0200

    feat: update product logic done

--- Wallafake/mutateProduct/mutateProductController.js
+++ Wallafake/mutateProduct/mutateProductController.js
@@ -1,4 +1,8 @@
-import { createProduct } from './mutateProductModel.js';
+import {
+  createProduct,
+  getProduct,
+  updateProduct,
+} from './mutateProductModel.js';
 import {
   dispatchCustomEvent,
   errorMessageEvent,
@@ -7,41 +11,84 @@ import {
   enableForm,
 } from '../utils/index.js';
 
-export const createProductController = (form) => {
+export const createProductController = async (form, productId) => {
   const token = localStorage.getItem('accessToken');
-
   checkAuth(token, form);
 
-  form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const data = getCreateProductFormData(formData);
+  if (productId) {
+    const product = await getProduct(productId);
 
-    try {
-      dispatchCustomEvent('createProductStart', null, form);
-      disableForm(form);
-      await createProduct(data, token);
-      successMessageEvent('create-product', 'Product created', form);
-      form.reset();
-      setTimeout(() => {
-        window.location.href = '/';
-      }, 1000);
-    } catch (error) {
-      errorMessageEvent('create-product', error.message, form);
-    } finally {
-      enableForm(form);
-      dispatchCustomEvent('createProductEnd', null, form);
+    for (const key of Object.keys(product)) {
+      const selectElement = form.querySelector(`[name="${key}"]`);
+      if (key === 'for') {
+        selectOption(selectElement, product, key);
+      } else {
+        selectElement.value = product[key];
+      }
     }
-  });
+
+    form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const formData = new FormData(form);
+      const data = getProductFormData(formData);
+      try {
+        dispatchCustomEvent('updateProductStart', null, form);
+        disableForm(form);
+        await updateProduct(data, token, productId);
+        successMessageEvent('update-product', 'Product updated', form);
+        setTimeout(() => {
+          window.location.href = '/';
+        }, 1000);
+      } catch (error) {
+        errorMessageEvent('update-product', error.message, form);
+      } finally {
+        enableForm(form);
+        dispatchCustomEvent('updateProductEnd', null, form);
+      }
+    });
+  }
+
+  if (!productId) {
+    form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const formData = new FormData(form);
+      const data = getProductFormData(formData);
+      try {
+        dispatchCustomEvent('createProductStart', null, form);
+        disableForm(form);
+        await createProduct(data, token);
+        successMessageEvent('create-product', 'Product created', form);
+        form.reset();
+        setTimeout(() => {
+          window.location.href = '/';
+        }, 1000);
+      } catch (error) {
+        errorMessageEvent('create-product', error.message, form);
+      } finally {
+        enableForm(form);
+        dispatchCustomEvent('createProductEnd', null, form);
+      }
+    });
+  }
 };
 
-const getCreateProductFormData = (formData) => {
+const selectOption = (selectElement, product, key) => {
+  const options = selectElement.options;
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].value === product[key]) {
+      selectElement.selectedIndex = i;
+      return;
+    }
+  }
+};
+
+const getProductFormData = (formData) => {
   return {
     image: formData.get('image'),
     name: formData.get('name'),
     description: formData.get('description'),
     price: +formData.get('price'),
-    for: formData.get('type'),
+    for: formData.get('for'),
   };
 };
 
--- Wallafake/mutateProduct/mutateProductModel.js
+++ Wallafake/mutateProduct/mutateProductModel.js
@@ -5,3 +5,25 @@ export const createProduct = async (product, token) => {
 
   await customFetch.post(url, { body: product, auth: `Bearer ${token}` });
 };
+
+export const getProduct = async (id) => {
+  const url = `/api/products/${id}?_expand=user`;
+  const data = await customFetch.get(url);
+  const product = {
+    name: data.name,
+    image: data.image,
+    description: data.description,
+    price: data.price,
+    for: data.for,
+  };
+  return product;
+};
+
+export const updateProduct = async (product, token, id) => {
+  const url = `/api/products/${id}`;
+  const response = await customFetch.patch(url, {
+    body: product,
+    auth: `Bearer ${token}`,
+  });
+  return response;
+};
