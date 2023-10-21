c1503f18ef0df270504d5baf8d95991eb244a989
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-21 18:22:04 +0200

    style: add tabulation

--- Wallafake/productDetail/productDetailView.js
+++ Wallafake/productDetail/productDetailView.js
@@ -5,10 +5,10 @@ export const buildProduct = (product) => {
       product.image
     }" alt="product image"  class="img" onerror="this.src='../assets/images/no-image-available.webp'"/>
       <div class="product-content">
-      <span>Name: ${product.name}</span>
-      <span>Description: ${product.description}</span>
-      <p>Price: ${product.price}€</p>
-      <p class=${product.for.toLowerCase()}>${product.for}</p>
+        <span>Name: ${product.name}</span>
+        <span>Description: ${product.description}</span>
+        <p>Price: ${product.price}€</p>
+        <p class=${product.for.toLowerCase()}>${product.for}</p>
       </div>
   </div>
     `;
