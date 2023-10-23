b4bedf430e6ad7380d2e24e97cd2ea978a053759
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-23 19:19:36 +0200

    feat: notifications documentation

--- Wallafake/notifications/notificationsController.js
+++ Wallafake/notifications/notificationsController.js
@@ -1,10 +1,31 @@
+// Import the function for building notifications.
 import { buildNotification } from './notificationsView.js';
 
+/**
+ * Notification Controller Function
+ *
+ * This function creates and displays notifications with specified messages and types.
+ *
+ * @param {HTMLElement} notifications - The container for displaying notifications.
+ *
+ * @returns {function} - A function that shows notifications with specified messages and types.
+ */
+
 export const notificationController = (notifications) => {
+  /**
+   * Show Notification Function
+   *
+   * This inner function creates and displays a notification with the given message and type.
+   *
+   * @param {string} message - The message to be displayed in the notification.
+   * @param {string} type - The type of the notification (e.g., 'success', 'error', etc.).
+   */
   const showNotification = (message, type) => {
     const notificationContainer = document.createElement('div');
     notificationContainer.innerHTML = buildNotification(message, type);
+
     notifications.appendChild(notificationContainer);
+
     setTimeout(() => {
       notificationContainer.innerHTML = '';
     }, 2000);
--- Wallafake/notifications/notificationsView.js
+++ Wallafake/notifications/notificationsView.js
@@ -1,7 +1,17 @@
+/**
+ * Build Notification Function
+ *
+ * This function generates an HTML representation of a notification with a specified message and type.
+ *
+ * @param {string} message - The message to be displayed in the notification.
+ * @param {string} type - The type of the notification (e.g., 'success', 'error', etc.).
+ *
+ * @returns {string} - The HTML code representing the notification.
+ */
 export const buildNotification = (message, type) => {
   return `
-  <div class="notification ${type}">
-    <p>${message}</p>
-  </div>
+    <div class="notification ${type}">
+      <p>${message}</p>
+    </div>
   `;
 };
