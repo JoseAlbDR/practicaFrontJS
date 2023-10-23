dcc68abe3ab276d0c42f4ba9232f8b2151a228ae
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-23 16:46:32 +0200

    feat: move all config variables to config/const.js

---
+++ Wallafake/config/const.js
@@ -0,0 +1,5 @@
+export const LIMIT = 5;
+export const BASE_URL = 'http://localhost:8000';
+export const LOGIN_ENDPOINT = '/auth/login';
+export const SIGNUP_ENDPOINT = '/auth/register';
+export const RESOURCE_ENDPOINT = '/api/products';
