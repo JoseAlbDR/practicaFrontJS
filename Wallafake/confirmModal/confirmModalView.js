2a0d97656e3681d5100658e819c7419b7ac8fcd0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-23 17:34:44 +0200

    feat: add comments to confirmModalView

--- Wallafake/confirmModal/confirmModalView.js
+++ Wallafake/confirmModal/confirmModalView.js
@@ -1,13 +1,21 @@
+/**
+ * Build Modal Function
+ *
+ * This function generates the HTML structure for a confirmation modal.
+ * It includes a message, information about irreversible actions, and confirm/cancel buttons.
+ *
+ * @returns {string} - The HTML structure for the confirmation modal as a string.
+ */
 export const buildModal = () => {
   return `
     <div class="modal-content">
       <header class="modal-header">
         <h5 id="modal-message"></h5>
-        <p>This operation cant be undone</p>
+        <p>This operation can't be undone</p>
       </header>
       <div class="modal-buttons">
         <button type="button" class="btn btn-block danger-btn" id="confirm-btn">Accept</button>
-        <button type="button" class="btn btn-block " id="cancel-btn">Cancel</button>
+        <button type="button" class="btn btn-block" id="cancel-btn">Cancel</button>
       </div>
     </div>
   `;
