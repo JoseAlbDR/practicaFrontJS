86b547be2519e4414e7c145aa37f82c599385e50
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-23 18:20:59 +0200

    feat: document menu

--- Wallafake/menu/menuController.js
+++ Wallafake/menu/menuController.js
@@ -1,27 +1,54 @@
 import { buildMenuItem } from './menuView.js';
 import { authenticatedItems, unauthenticatedItems } from './menuModel.js';
 
+/**
+ * Menu Controller Function
+ *
+ * This function dynamically renders the menu items in the provided menu container based on the user's authentication status.
+ *
+ * @param {HTMLElement} menuContainer - The HTML element that will contain the menu items.
+ * @param {string} activeItem - The identifier for the currently active menu item.
+ */
 export const menuController = (menuContainer, activeItem) => {
+  // Authenticated menu items
   if (localStorage.getItem('accessToken')) {
     renderMenuItems(menuContainer, authenticatedItems, activeItem);
     logoutEvent();
   } else {
+    // Not Authenticated
     renderMenuItems(menuContainer, unauthenticatedItems, activeItem);
   }
 };
 
+/**
+ * Render Menu Items Function
+ *
+ * This function generates the HTML structure for menu items and appends them to the menu container.
+ *
+ * @param {HTMLElement} menuContainer - The HTML element that will contain the menu items.
+ * @param {Array} menuItems - An array of menu items to be rendered.
+ * @param {string} activeItem - The identifier for the currently active menu item.
+ */
 const renderMenuItems = (menuContainer, menuItems, activeItem) => {
   const menuLinks = document.createElement('div');
   menuLinks.classList.add('menu-items');
+
   menuItems.forEach((item) => {
     const menuItem = buildMenuItem(activeItem, item);
     menuLinks.appendChild(menuItem);
   });
+
   menuContainer.appendChild(menuLinks);
 };
 
+/**
+ * Logout Event Function
+ *
+ * This function adds a click event listener to the logout button, allowing the user to log out by removing the access token from local storage.
+ */
 const logoutEvent = () => {
   const logout = document.getElementById('logout');
+
   logout.addEventListener('click', () => {
     localStorage.removeItem('accessToken');
   });
--- Wallafake/menu/menuView.js
+++ Wallafake/menu/menuView.js
@@ -1,8 +1,19 @@
+/**
+ * Build Menu Item Function
+ *
+ * This function creates an HTML menu item element based on the provided data.
+ *
+ * @param {string} activeItem - The identifier of the currently active menu item.
+ * @param {object} item - An object representing the menu item with properties such as 'name', 'href', and 'id'.
+ *
+ * @returns {HTMLAnchorElement} - The HTML anchor element representing the menu item.
+ */
 export const buildMenuItem = (activeItem, item) => {
   const menuItem = document.createElement('a');
   menuItem.textContent = item.name;
   menuItem.href = item.href;
   menuItem.id = item.id;
   activeItem === item.id ? (menuItem.className = 'active') : null;
+
   return menuItem;
 };
