4ba921e4578b6e27647ab2d29f6026745cc6cf9e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-23 20:44:04 +0200

    feat: spinner component documentation

--- spinner/spinnerController.js
+++ spinner/spinnerController.js
@@ -1,9 +1,23 @@
 import { buildSpinner } from './spinnerView.js';
 
+/**
+ * Spinner Controller Function
+ *
+ * This function controls the display and removal of a loading spinner.
+ *
+ * @param {HTMLElement} spinner - The HTML element where the spinner will be displayed.
+ */
 export const spinnerController = (spinner) => {
+  /**
+   * Display the loading spinner.
+   */
   const showSpinner = () => {
     spinner.innerHTML = buildSpinner();
   };
+
+  /**
+   * Remove the loading spinner.
+   */
   const hideSpinner = () => {
     spinner.innerHTML = '';
   };
--- spinner/spinnerView.js
+++ spinner/spinnerView.js
@@ -1,3 +1,10 @@
+/**
+ * Build Spinner Function
+ *
+ * This function generates the HTML for a loading spinner.
+ *
+ * @returns {string} - A string containing the HTML for the loading spinner.
+ */
 export const buildSpinner = () => {
   return `<div class="loading"></div>`;
 };
