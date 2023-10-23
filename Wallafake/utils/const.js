25c3f6c180a91d55dbcabe5aaa43ffb16abf4a4d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-23 16:49:27 +0200

    refactor: validate filter params to his own function

--- Wallafake/utils/const.js
+++
@@ -1,3 +0,0 @@
-export const LIMIT = 5;
-export const LIMIT_PARAM = `_limit=${LIMIT}`;
-export const BASE_URL = 'http://localhost:8000';
--- Wallafake/utils/index.js
+++ Wallafake/utils/index.js
@@ -1,5 +1,5 @@
 export { customFetch } from './customFetch.js';
-export { BASE_URL, LIMIT, LIMIT_PARAM } from './const.js';
+export { BASE_URL, LIMIT } from '../config/const.js';
 export { disableForm } from './disableForm.js';
 export { enableForm } from './enableForm.js';
 export { decodeToken } from './decodeToken.js';
@@ -10,3 +10,4 @@ export {
 } from './customEvent.js';
 export { getSearchParams } from './getSearchParams.js';
 export { selectDefaultOption } from './selectDefaultOption.js';
+export { filterParams } from './filterParams.js';
